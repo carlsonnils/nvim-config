79d3efb867483eacd23b3dd103e71a4011a78d8b
Nils <carlsonnils@gmail.com>
2024-06-21 00:51:16 -0400

    first commit

---
+++ init.lua
@@ -0,0 +1,13 @@
+-- include plugins
+require("plugins")
+
+-- tab spacing
+-- set tab to 4 spaces
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.smarttab = true
+
+-- set color scheme
+vim.cmd("colorscheme tender")
+
+
